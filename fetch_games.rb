a4ebf214aeb6ec3bdd0918878368d20cb0e5eab2
stenno <c.stegen@jungundwillich.de>
2011-06-15 01:44:21 +0200

    fetch_games errors fixed

--- fetch_games.rb
+++ fetch_games.rb
@@ -5,7 +5,16 @@ require 'parse'
 def fetch_all
 	for server in Server.all
 		puts "server #{server.name} start!"
+		puts "url #{server.xlogurl}"
 		header = XLog.parse_header XLog.fetch_header(server.xlogurl)
+		puts "fetched header #{header.inspect}"
+		puts "current offset: #{server.xlogcurrentoffset}"
+		if server.xlogcurrentoffset == nil
+			server.xlogcurrentoffset = header['Content-Length'].to_i
+			server.xloglastmodified = header['Last-Modified']
+			server.save
+			return
+		end
 		if DateTime.parse(server.xloglastmodified) < DateTime.parse(header['Last-Modified'])
 			puts "fetching games ...."
 			if games = XLog.fetch_from_xlog(server.xlogurl, server.xlogcurrentoffset, header['Content-Length'])
@@ -22,6 +31,8 @@ def fetch_all
 			end
 			server.xloglastmodified = header['Last-Modified']
 			server.save
+		else	
+			puts "no new games, try again later"
 		end
 	end	
 end
