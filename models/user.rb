12dd7de28fd8390be86f53d601210ebe45721bc4
Mikko Juola <mikjuo@gmail.com>
2011-06-29 21:02:58 +0300

    Revert "Use scrypt."
    This reverts commit a0a0ed4eab93ab39dd338b31f6576dc1c1e0d0c8.

--- models/user.rb
+++ models/user.rb
@@ -8,16 +8,18 @@ class User
 
     property :id,     Serial
     property :login,  String
-    property :hashed, String, :length => 200
+    property :hashed, String, :length => 64
+    property :salt,   String, :length => 64
 
     def password=(pw)
-        self.hashed = SCrypt::Password.create(pw, :max_time => 0.5)
+        self.salt = Digest::SHA256.hexdigest("#{rand}") #generate random hash
+        self.hashed = User.encrypt(pw, self.salt)
     end 
 
     def self.authenticate(login, pass)
         u = User.first(:login => login)
         return false unless u
-        (SCrypt::Password.new(u.hashed) == pass) ? u : false
+        User.encrypt(pass, u.salt) == u.hashed ? u : false
     end
 
     def games
