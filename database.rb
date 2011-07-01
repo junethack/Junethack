ccc341778a151e8ef1d0d22918e20eae65d3e640
Patric Mueller <bhaak@gmx.net>
2011-07-01 10:56:30 +0200

    throw exceptions when saves fail and add debug info for datamapper sql

--- database.rb
+++ database.rb
@@ -3,6 +3,12 @@ require 'data_mapper'
 require 'dm-serializer'
 $dbname = "junethack.db"
 
+# for debugging: print all generated SQL statemtens
+#DataMapper::Logger.new(STDOUT, :debug)
+
+# raise exception on error when saving
+DataMapper::Model.raise_on_save_failure = true # globally
+
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/#{$dbname}")
 
 require 'models/server'
