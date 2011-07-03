b3b1956a5da534fad1e6b349987d0d8755921d66
Patric Mueller <bhaak@gmx.net>
2011-07-03 22:14:10 +0200

    enlarge strings for server urls

--- models/server.rb
+++ models/server.rb
@@ -6,9 +6,9 @@ class Server
     has n, :users, :through => :accounts
 
     property :id,                Serial
-    property :name,              String
-    property :url,               String
-    property :xlogurl,           String
+    property :name,              String, :length => 255
+    property :url,               String, :length => 255
+    property :xlogurl,           String, :length => 255
     property :xloglastmodified,  String
     property :variant,           String
     property :xlogcurrentoffset, Integer
