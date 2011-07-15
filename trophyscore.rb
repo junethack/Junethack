06ae8e2d79ef5e3812324d70d97c2079fb7f9537
Patric Mueller <bhaak@gmx.net>
2011-07-15 11:00:57 +0200

    Fix spurious unique constraint exceptions when calculitaing trophies

--- trophyscore.rb
+++ trophyscore.rb
@@ -3,15 +3,15 @@ require 'helper'
 class TrophyScore
 
 def most_ascensions(variant=nil)
-    return (repository.adapter.select "select * from (select count(1) as ascension, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' group by user_id) where ascension = (select max(max_ascension) from (select count(1) as max_ascension from games where version = ? and user_id is not null and ascended='t' group by user_id));", variant, variant)
+    return (repository.adapter.select "select distinct * from (select count(1) as ascension, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' group by user_id) where ascension = (select max(max_ascension) from (select count(1) as max_ascension from games where version = ? and user_id is not null and ascended='t' group by user_id));", variant, variant)
 end
 
 def highest_scoring_ascension(variant=nil)
-    return repository.adapter.select "select points, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' and points = (select max(points) from games where version = ? and user_id is not null and ascended='t') order by endtime", variant, variant
+    return repository.adapter.select "select distinct points, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' and points = (select max(points) from games where version = ? and user_id is not null and ascended='t') order by endtime", variant, variant
 end
 
 def lowest_scoring_ascension(variant=nil)
-    return repository.adapter.select "select points, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' and points = (select min(points) from games where version = ? and user_id is not null and ascended='t') order by endtime", variant, variant
+    return repository.adapter.select "select distinct points, user_id, (select name from users where user_id = id) as user, endtime from games where version = ? and user_id is not null and ascended='t' and points = (select min(points) from games where version = ? and user_id is not null and ascended='t') order by endtime", variant, variant
 end
 
 def most_conducts_ascension(variant=nil)
