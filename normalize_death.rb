d70ed792fd94a5cecfb0308a445b932dc6c9b248
Patric Mueller <bhaak@gmx.net>
2011-07-31 20:29:00 +0200

    Filter deaths for unique deaths

--- normalize_death.rb
+++ normalize_death.rb
@@ -1,5 +1,22 @@
 class Game
   def normalize_death
-    return self.death
+    # no need to be grammatical correct
+    death = self.death.gsub /^killey by an /, "killed by a "
+
+    death = death.gsub /, while .*/, ""
+
+    death = death.gsub /hallucinogen-distorted /, ""
+
+    death = death.gsub /by the invisible /, "by "
+    death = death.gsub /by (an|a) invisible /, "by a "
+    death = death.gsub /by invisible /, "by "
+
+    death = death.gsub(/ (her|his) /, " eir ")
+    death = death.gsub(/ (herself|himself) /, " eirself ")
+    death = death.gsub(/ (herself|himself)$/, " eirself")
+
+    death = death.gsub(/ (called|named) .*/, "")
+
+    death = death.gsub(/ \(with the Amulet\)$/, "")
   end
 end
