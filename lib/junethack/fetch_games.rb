9a7225728789b2189563cff9a0b99805ba2cddeb
Patric Mueller <bhaak@gmx.net>
2013-05-05 11:07:15 +0200

    Better differentiate fetch games log messages into debug and info

--- lib/junethack/fetch_games.rb
+++ lib/junethack/fetch_games.rb
@@ -17,6 +17,7 @@ def fetch_all
       @fetch_logger.info "File #{@stop_fetching_games} exists, don't get new games."
       return
     end
+    @fetch_logger.info "Looking for new games..."
     for server in Server.all
       begin
         count_games = 0
@@ -24,11 +25,11 @@ def fetch_all
         count_junk_games = 0
         count_non_tournament_games = 0
 
-        @fetch_logger.info "server #{server.name} start!"
-        @fetch_logger.info "url #{server.xlogurl}"
+        @fetch_logger.debug "server #{server.name} start!"
+        @fetch_logger.debug "url #{server.xlogurl}"
         header = XLog.parse_header XLog.fetch_header(server.xlogurl)
-        @fetch_logger.info "fetched header #{header.inspect}"
-        @fetch_logger.info "current offset: #{server.xlogcurrentoffset}"
+        @fetch_logger.debug "fetched header #{header.inspect}"
+        @fetch_logger.debug "current offset: #{server.xlogcurrentoffset}"
         if server.xlogcurrentoffset == nil
             server.xlogcurrentoffset = header['Content-Length'].to_i
             server.xloglastmodified = header['Last-Modified'] || 'Thu, 01 Jan 1970 00:00:00 GMT'
@@ -39,12 +40,12 @@ def fetch_all
         # in case the web server doesn't send Last-Modified, use current time
         # because of byte-range fetching, not much overhead is generated
         last_modified = header['Last-Modified'] || Time.now.httpdate
-        @fetch_logger.info "last-modified #{last_modified}"
+        @fetch_logger.debug "last-modified #{last_modified}"
         if DateTime.parse(server.xloglastmodified) < DateTime.parse(last_modified)
-            @fetch_logger.info "fetching games ...."
+            @fetch_logger.debug "fetching games ...."
             if gamesIO = XLog.fetch_from_xlog(server.xlogurl, server.xlogcurrentoffset, header['Content-Length'])
                 games = gamesIO.readlines
-                @fetch_logger.info "So many games ... #{games.length}"
+                @fetch_logger.info "#{games.length} new game#{'s' if games.length != 1} on #{server.name}."
                 i = 0
                     games.each do |line|
                       begin
@@ -61,11 +62,11 @@ def fetch_all
                             regular_game = false
                             if hgame['turns'].to_i <= 10 and ['escaped','quit'].include? hgame['death'] then
                                 game = StartScummedGame.create(hgame.merge({"server" => server}))
-                                @fetch_logger.info "start scummed game"
+                                @fetch_logger.debug "start scummed game"
                                 count_scummed_games += 1
                             elsif hgame['mode'] == 'explore' or hgame['mode'] == 'multiplayer' then
                                 game = JunkGame.create(hgame.merge({"server" => server}))
-                                @fetch_logger.info "junk game"
+                                @fetch_logger.debug "junk game"
                                 count_junk_games += 1
                             else
                                 game = Game.create(hgame.merge({"server" => server}))
@@ -90,13 +91,13 @@ def fetch_all
                             end
 
                             if game.save
-                                @fetch_logger.info "created #{i}"
+                                @fetch_logger.debug "created #{i}"
                             else
                                 raise "something went wrong, could not create games"
                             end
 
                         else
-                            @fetch_logger.info "not part of tournament #{i}"
+                            @fetch_logger.debug "not part of tournament #{i}"
                             count_non_tournament_games += 1
                         end
                         # this game is completely input into the db
@@ -109,14 +110,14 @@ def fetch_all
                       end
                     end
                 raise "xlogcurrentoffset mismatch: #{server.xlogcurrentoffset} != #{header['Content-Length'].to_i}" if server.xlogcurrentoffset != header['Content-Length'].to_i
-                @fetch_logger.info "Inserted #{count_games} tournament, #{count_non_tournament_games} non tournament, #{count_scummed_games} start scummed, and #{count_junk_games} junk games."
+                @fetch_logger.info "Inserted #{count_games} tournament, #{count_non_tournament_games} non tournament, #{count_scummed_games} start scummed, and #{count_junk_games} junk games on #{server.name}."
             else
-                @fetch_logger.info "No games at all!"
+                @fetch_logger.debug "No games at all on #{server.name}!"
             end
             server.xloglastmodified = last_modified
             $db_access.synchronize { server.save }
         else
-            @fetch_logger.info "no new games, try again later"
+            @fetch_logger.debug "No new games on #{server.name}."
         end
       rescue Exception => e
           @fetch_logger.error e.to_s
