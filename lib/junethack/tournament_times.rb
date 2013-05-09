35bc55cff33c7e601bd8287dbae9af04b2122d1a
Patric Mueller <bhaak@gmx.net>
2013-05-09 13:36:46 +0200

    Move signup start time to one week before tournaments starts

--- lib/junethack/tournament_times.rb
+++ lib/junethack/tournament_times.rb
@@ -3,5 +3,5 @@ require 'time'
 
 $tournament_starttime = Time.parse("2013-06-01 00:00:00Z").to_i
 $tournament_endtime   = Time.parse("2013-07-01 00:00:00Z").to_i
-$tournament_signupstarttime = Time.parse("2013-05-01 00:00:00Z").to_i
+$tournament_signupstarttime = Time.parse("2013-05-25 00:00:00Z").to_i
 
