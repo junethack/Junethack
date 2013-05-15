2525af3a4d3cb170a1299f31caf1a571c95519b5
Patric Mueller <bhaak@gmx.net>
2013-05-15 22:25:59 +0200

    Add archive of 2011 and 2012 tournament results

---
+++ public/archive/2012/graph/graph_ascensions.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	var options = {
+		chart: {
+			renderTo: 'chart_ascensions',
+			type: 'column'
+		},
+		title: {
+			text: 'Ascensions per Day'
+		},
+		xAxis: {
+			type: 'datetime',
+		},
+		yAxis: {
+			title: {
+				text: 'Ascensions'
+			},
+		},
+		series: [],
+		tooltip: {
+			shared: true,
+		},
+		legend: {
+			enabled: false
+		},
+	};
+	
+	/* Load the data from the CSV file. */
+	$.get('/tmp/activity.csv', function(data) {
+		// Split the lines
+		var lines = data.split('\n');
+		$.each(lines, function(lineNo, line) {
+			if (lineNo == 0) {
+			var items = line.split(',');
+			var series = {
+				data: [],
+				pointStart: Date.UTC(2012, 05, 01), // 2012-06-01
+				pointInterval: 24 * 3600 * 1000, // one day
+			};
+			$.each(items, function(itemNo, item) {
+				series.data.push(parseFloat(item));
+			});
+			
+			options.series.push(series);
+			}
+		});
+		
+		var chart = new Highcharts.Chart(options);
+	});
+	
+	
+});
