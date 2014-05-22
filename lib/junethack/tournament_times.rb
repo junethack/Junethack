9477f6807704a9e93e47909efd9ac6de93972e7d
Patric Mueller <bhaak@gmx.net>
2014-05-22 19:22:49 +0200

    Correct signup time for 2014

--- lib/junethack/tournament_times.rb
+++ lib/junethack/tournament_times.rb
@@ -3,5 +3,5 @@ require 'time'
 
 $tournament_starttime = Time.parse("2014-06-01 00:00:00Z").to_i
 $tournament_endtime   = Time.parse("2014-07-01 00:00:00Z").to_i
-$tournament_signupstarttime = Time.parse("2014-05-01 00:00:00Z").to_i
+$tournament_signupstarttime = Time.parse("2014-05-19 00:00:00Z").to_i
 
