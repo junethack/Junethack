ef34bb7edc7dcb417b23d729ffcfa8081b2e42a8
Patric Mueller <bhaak@gmx.net>
2015-05-10 16:42:25 +0200

    Update date and tournament dates for 2015

--- lib/junethack/tournament_times.rb
+++ lib/junethack/tournament_times.rb
@@ -1,7 +1,7 @@
 require 'date'
 require 'time'
 
-$tournament_starttime = Time.parse("2014-06-01 00:00:00Z").to_i
-$tournament_endtime   = Time.parse("2014-07-01 00:00:00Z").to_i
-$tournament_signupstarttime = Time.parse("2014-05-19 00:00:00Z").to_i
+$tournament_starttime = Time.parse("2015-06-01 00:00:00Z").to_i
+$tournament_endtime   = Time.parse("2015-07-01 00:00:00Z").to_i
+$tournament_signupstarttime = Time.parse("2015-05-10 00:00:00Z").to_i
 
