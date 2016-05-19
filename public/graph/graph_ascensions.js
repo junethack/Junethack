131314eafe83c45243d49a7c19f1383104e990ff
Patric Mueller <bhaak@gmx.net>
2016-05-20 00:13:25 +0200

    Update date and tournament dates for 2016

--- public/graph/graph_ascensions.js
+++ public/graph/graph_ascensions.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 			var items = line.split(',');
 			var series = {
 				data: [],
-				pointStart: Date.UTC(2015, 05, 01), // 2015-06-01
+				pointStart: Date.UTC(2016, 05, 01), // 2016-06-01
 				pointInterval: 24 * 3600 * 1000, // one day
 			};
 			$.each(items, function(itemNo, item) {
