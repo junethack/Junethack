8471d470c3a087897b71cd719054b7132fbbf4d7
Patric Mueller <bhaak@gmx.net>
2025-05-31 23:41:49 +0200

    Update registration date for 2025

--- lib/junethack/tournament_times.rb
+++ lib/junethack/tournament_times.rb
@@ -3,6 +3,6 @@ require 'time'
 
 $tournament_starttime       = Time.parse("2025-06-01 00:00:00Z").to_i
 $tournament_endtime         = Time.parse("2025-07-01 00:00:00Z").to_i
-# Monday before Junethack starts
-# $tournament_signupstarttime = Time.parse("2025-05-26 00:00:00Z").to_i
+# Monday before Junethack starts, ahem
+$tournament_signupstarttime = Time.parse("2025-05-31 00:00:00Z").to_i
 $tournament_year = Time.at($tournament_starttime).year
